784701a6555dcdbc32bebe11d89de00451dd9224
aangairbender <gvoin16@gmail.com>
2023-05-22 00:31:14 +0900

    Add project files.

---
+++ cg-singlefile-for-cpp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace cg_singlefile_for_cpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new CommandLineApplication();
+            var dirArg = cmd.Option("-d | --dir <value>", "directory with the main file", CommandOptionType.SingleValue);
+            var mainArg = cmd.Option("-m | --main <value>", "main file name (relative)", CommandOptionType.SingleValue);
+            var outputArg = cmd.Option("-o | --output <value>", "output file", CommandOptionType.SingleValue);
+
+            cmd.OnExecute(() =>
+            {
+                if (!dirArg.HasValue() || !mainArg.HasValue() || !outputArg.HasValue())
+                {
+                    Console.WriteLine("Invalid arguments. See --help for usage guide.");
+                    return -1;
+                }
+                var sourceProcessor = new SourceProcessor(new System.IO.DirectoryInfo(dirArg.Value()));
+                sourceProcessor.Process(mainArg.Value(), outputArg.Value());
+                return 0;
+            });
+
+            cmd.HelpOption("-h | --help");
+            cmd.Execute(args);
+        }
+    }
+}
---
+++ cg-singlefile-for-cpp/SourceProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace cg_singlefile_for_cpp
+{
+    public class SourceProcessor
+    {
+        private readonly DirectoryInfo sourceDir;
+
+        private readonly Regex includeSrcRegex = new Regex("#include \"(.*)\"");
+        private readonly Regex includeLibRegex = new Regex("#include <(.*)>");
+
+        private readonly HashSet<string> includedSrc = new HashSet<string>();
+        private readonly HashSet<string> includedLib = new HashSet<string>();
+
+        public SourceProcessor(DirectoryInfo sourceDir)
+        {
+            this.sourceDir = sourceDir;
+        }
+
+        public void Process(string rootFilename, string outputFile)
+        {
+            includedSrc.Clear();
+            includedLib.Clear();
+            var rootFile = new FileInfo(Path.Combine(sourceDir.FullName, rootFilename));
+            var lines = UnfoldIncludes(rootFile);
+            File.WriteAllLines(outputFile, lines);
+        }
+
+        private List<string> UnfoldIncludes(FileInfo file)
+        {
+            includedSrc.Add(file.FullName);
+            var result = new List<string>();
+            var lines = ReadLines(file.FullName);
+            foreach (var line in lines)
+            {
+                if (line.EndsWith("singlefile-skip-line")) continue;
+
+                if (line.TrimStart().StartsWith("//")) continue;
+
+                if (IsInclude(line, includeSrcRegex, out var includeFilename))
+                {
+                    var includeFile = new FileInfo(Path.Combine(file.DirectoryName, includeFilename));
+                    if (includedSrc.Contains(includeFile.FullName)) continue;
+                    result.AddRange(UnfoldIncludes(includeFile));
+                }
+                else if (IsInclude(line, includeLibRegex, out var includeLibFile))
+                {
+                    if (includedLib.Contains(includeLibFile)) continue;
+                    includedLib.Add(includeLibFile);
+                    result.Add(line);
+                }
+                else if (IsPragmaOnce(line))
+                {
+                    continue;
+                }
+                else
+                {
+                    result.Add(line);
+                }
+            }
+            return result;
+        }
+
+        private static bool IsPragmaOnce(string line)
+        {
+            return line.Trim() == "#pragma once";
+        }
+
+        private static bool IsInclude(string line, Regex regex, out string includeFile)
+        {
+            var includeMatch = regex.Match(line);
+            if (includeMatch.Success)
+            {
+                includeFile = includeMatch.Groups[1].Value;
+                return true;
+            } else
+            {
+                includeFile = null;
+                return false;
+            }
+        }
+
+        private string[] ReadLines(string filepath)
+        {
+            return File.ReadAllLines(filepath);
+        }
+    }
+}
